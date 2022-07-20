72cb0245c7c3a57fdedd5e334f827353840e1d93
georgolden <georgoldenb@gmail.com>
2022-07-20 15:02:12 +0200

    Bug reproduce Version 1.1.3

--- testPlugin.js
+++ testPlugin.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-loop-func */
 'use strict';
 
 const crud = (init) => () => {
   const iface = {};
   const { entities } = init;
   for (const [entity, data] of Object.entries(entities)) {
-    iface['schema' + entity] = async () => data;
+    iface['schema' + entity] = async () => ({ context, data });
   }
   return iface;
 };
