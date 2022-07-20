1c1b33f139267a080f303ba6184769857849b2f1
georgolden <georgoldenb@gmail.com>
2022-07-20 14:46:40 +0200

    Bugfix

--- testPlugin.js
+++ testPlugin.js
@@ -6,6 +6,7 @@ const crud = (init) => {
   for (const [entity, data] of Object.entries(entities)) {
     iface['schema' + entity] = async () => data;
   }
+  return iface;
 };
 
 module.exports = { plugins: { crud } };
